swt-initiative31/prototype-skija

Improve Image support: ImageData <-> Skija Image
- ImageUtils for setting a new ImageDataProvider for an existing image
and for creating generic images.
- SkijaImageDataProvider as ImageDataProvider which can scale imageData
- package protected Image method setImageDataProvider in order to
prevent, an API change.
- package protected Image constructor to prevent API-change.
- Add generic image handling also to MacOS implementation
- improve the image handling in SkijaGC. ImageData should contain as
default RGB + alphaData. In contrast to Skija Image, which contains
usually RGBA_8888. Make the converion work more generally.

Signed-off-by: Denis Ungemach <denis.ungemach@sap.com>

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Image.java
@@ -159,6 +159,8 @@ public final class Image extends Resource implements Drawable {
 
 	private HashMap<Integer, Long> zoomLevelToHandle = new HashMap<>();
 
+	private boolean genericImage;
+
 /**
  * Prevents uninitialized instances from being created outside the package.
  */
@@ -1370,6 +1372,12 @@ public Rectangle getBoundsInPixels() {
 }
 
 private Rectangle getBoundsInPixelsFromNative() {
+
+	if (this.genericImage) {
+		var d = imageDataProvider.getImageData(100);
+		return new Rectangle(0, 0, d.width, d.height);
+	}
+
 	switch (type) {
 		case SWT.BITMAP:
 			BITMAP bm = new BITMAP();
@@ -1442,6 +1450,9 @@ public ImageData getImageData() {
  * @since 3.106
  */
 public ImageData getImageData (int zoom) {
+	if (this.genericImage)
+	    return imageDataProvider.getImageData(zoom);
+
 	if (isDisposed()) SWT.error(SWT.ERROR_GRAPHIC_DISPOSED);
 	int currentZoom = getZoom();
 	if (zoom == currentZoom) {
@@ -1494,6 +1505,10 @@ public ImageData getImageData (int zoom) {
  */
 @Deprecated
 public ImageData getImageDataAtCurrentZoom() {
+
+	if (this.genericImage)
+		return imageDataProvider.getImageData(100);
+
 	if (isDisposed()) SWT.error(SWT.ERROR_GRAPHIC_DISPOSED);
 	BITMAP bm;
 	int depth, width, height;
@@ -2300,6 +2315,10 @@ public void internal_dispose_GC (long hDC, GCData data) {
  */
 @Override
 public boolean isDisposed() {
+
+	if(this.genericImage)
+		return false;
+
 	return handle == 0;
 }
 
@@ -2424,5 +2443,24 @@ private class StaticZoomUpdater implements AutoCloseable {
 			DPIUtil.setDeviceZoom(currentNativeDeviceZoom);
 		}
 	}
+}
+
+void setImageDataProvider(ImageDataProvider imgDataProv) {
+    if (!this.isDisposed())
+	dispose();
+    this.genericImage = true;
+    this.imageDataProvider = imgDataProv;
+
+}
+
+/**
+ * constructor with imageData only in order to prevent conversion because of the
+ * device. This constructor is mainly for the usage with Skija. Don't use this
+ *
+ * @param res
+ */
+Image(ImageDataProvider imgDataProvider) {
+    this.genericImage = true;
+    this.imageDataProvider = imgDataProvider;
 }
 }

HeikoKlare/eclipse.platform.swt

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Image.java
@@ -123,6 +123,8 @@ public final class Image extends Resource implements Drawable {
 
 	private Map<Integer, ImageHandle> zoomLevelToImageHandle = new HashMap<>();
 
+	private boolean genericImage;
+
 /**
  * Prevents uninitialized instances from being created outside the package.
  */
@@ -1321,6 +1323,11 @@ public Rectangle getBounds() {
 }
 
 Rectangle getBounds(int zoom) {
+	if (this.genericImage) {
+		var d = imageProvider.getImageData(100);
+		return new Rectangle(0, 0, d.width, d.height);
+	}
+
 	if (isDisposed()) SWT.error(SWT.ERROR_GRAPHIC_DISPOSED);
 	if (zoomLevelToImageHandle.containsKey(zoom)) {
 		ImageHandle imageMetadata = zoomLevelToImageHandle.get(zoom);
@@ -1405,6 +1412,9 @@ public ImageData getImageData() {
  * @since 3.106
  */
 public ImageData getImageData (int zoom) {
+	if (this.genericImage)
+		return imageProvider.getImageData(zoom);
+
 	if (isDisposed()) SWT.error(SWT.ERROR_GRAPHIC_DISPOSED);
 	if (zoomLevelToImageHandle.containsKey(zoom)) {
 		return zoomLevelToImageHandle.get(zoom).getImageData();
@@ -1444,6 +1454,9 @@ public ImageData getImageData (int zoom) {
  */
 @Deprecated
 public ImageData getImageDataAtCurrentZoom() {
+	if (Image.this.genericImage)
+		return imageProvider.getImageData(100);
+
 	return getImageMetadata(getZoom()).getImageData();
 }
 
@@ -1975,6 +1988,9 @@ public void internal_dispose_GC (long hDC, GCData data) {
  */
 @Override
 public boolean isDisposed() {
+	if(this.genericImage)
+		return false;
+
 	if (this.imageProvider != null) {
 		return this.imageProvider.isDisposed();
 	}
@@ -2620,5 +2636,23 @@ private class ImageHandle {
 		}
 	}
 
+}
+void setImageDataProvider(ImageDataProvider imgDataProv) {
+    if (!this.isDisposed())
+	dispose();
+    this.genericImage = true;
+	this.imageProvider = new ImageDataProviderWrapper(imgDataProv);
+
+}
+
+/**
+ * constructor with imageData only in order to prevent conversion because of the
+ * device. This constructor is mainly for the usage with Skija. Don't use this
+ *
+ * @param res
+ */
+Image(ImageDataProvider imgDataProvider) {
+    this.genericImage = true;
+	this.imageProvider = new ImageDataProviderWrapper(imgDataProvider);
 }
 }