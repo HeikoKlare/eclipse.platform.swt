018b9edde5c390d24f5595a94b1caf3fad42e4d7
Heiko Klare <heiko.klare@vector.com>
2025-05-29 10:51:05 +0200

    Demo Combo MacOS

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet1.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet1.java
@@ -13,6 +13,9 @@
  *******************************************************************************/
 package org.eclipse.swt.snippets;
 
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.layout.*;
 /*
  * example snippet: Hello World
  *
@@ -26,7 +29,17 @@ public class Snippet1 {
 public static void main (String [] args) {
 	Display display = new Display ();
 	Shell shell = new Shell(display);
-	shell.setText("Snippet 1");
+	shell.setText("Snippet");
+	shell.setLayout(new GridLayout());
+	Combo combo = new Combo(shell, SWT.DROP_DOWN);
+	combo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	combo.add("first");
+	combo.add("second");
+	combo.select(0);
+	System.out.println(combo.getSelectionIndex());
+	combo.addMouseListener(MouseListener.mouseDownAdapter(e -> System.out.println(combo.getSelectionIndex())));
+	Text text = new Text(shell, SWT.NONE);
+	text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 	shell.open ();
 	while (!shell.isDisposed ()) {
 		if (!display.readAndDispatch ()) display.sleep ();
